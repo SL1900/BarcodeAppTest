3e0ae6d8f70ee2135b634976dd68e1359e32e765
SL1900 <david_super@inbox.ru>
2024-01-16 22:06:55 +0900

    Formating

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	//
 	import { BarcodeDetector } from "barcode-detector/pure";
-    import { onMount } from "svelte";
+	import { onMount } from "svelte";
 
 	let detector = new BarcodeDetector();
 
@@ -10,59 +10,64 @@
 
 	let list = {};
 
-	async function startCam(){
-	   const stream = await navigator.mediaDevices.getUserMedia({
-	    video: {
-	      facingMode: "environment"
-	    }
-	  });
-	  vsource.srcObject = stream;
-	  vsource.play();
-	
-	  setInterval(async ()=>{
-	    //
-	    let track = stream
-	    	.getVideoTracks()[0]
-		.applyConstraints({
-		  focusMode: "continuous"
+	async function startCam() {
+		const stream = await navigator.mediaDevices.getUserMedia({
+			video: {
+				facingMode: "environment",
+			},
 		});
-	    let res = await detector.detect(vsource);
-	    //console.log(res);
-	    for(let match of res){
-	      //
-	      list[match.rawValue] = {
-		format: match.format,
-		value: match.rawValue,
-		time: Date.now()
-	      }
-	      console.log(match.format, match.rawValue);
-	    }
-	  }, 200);
+		vsource.srcObject = stream;
+		vsource.play();
+
+		setInterval(async () => {
+			//
+			let track = stream.getVideoTracks()[0].applyConstraints({
+				// @ts-ignore
+				focusMode: "continuous",
+			});
+			let res = await detector.detect(vsource);
+			//console.log(res);
+			for (let match of res) {
+				//
+				list[match.rawValue] = {
+					format: match.format,
+					value: match.rawValue,
+					time: Date.now(),
+				};
+				console.log(match.format, match.rawValue);
+			}
+		}, 200);
 	}
-	onMount(()=>{
-	  //
+	onMount(() => {
+		//
 	});
 </script>
 
 <main>
-	<video bind:this={vsource} width="200" height="300"/>
+	<video bind:this={vsource} width="200" height="300" />
 	<div on:click={startCam}>start</div>
-  <div>Test</div>
-  <h1>Welcome to SvelteKit</h1>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <div class="list">
-    {#each Object.entries(list) as i}
-	    <div class="item">{i[1].format}:{i[1].value}</div>
-    {/each}
-  </div>
+	<div>Test</div>
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	</p>
+	<div class="list">
+		{#each Object.entries(list) as i}
+			<div class="item">{i[1].format}:{i[1].value}</div>
+		{/each}
+	</div>
 </main>
 
 <style>
-  .item{
-    animation: ease 1s n_i;
-  }
-  @keyframes n_i{
-    0%{ background-color: rgb(0, 255, 0); }
-    100%{ background-color: rgb(255, 255, 255); }
-  }
+	.item {
+		animation: ease 1s n_i;
+	}
+	@keyframes n_i {
+		0% {
+			background-color: rgb(0, 255, 0);
+		}
+		100% {
+			background-color: rgb(255, 255, 255);
+		}
+	}
 </style>
